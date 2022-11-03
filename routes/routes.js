f7e14b8aac1c149f073ae4348d316bb85e31fa9a
RageKage <99547837+RageKage@users.noreply.github.com>
2022-11-03 16:42:42 -0500

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+let express = require('express')
+let router = express.Router()
+
+router.get('/', function(req, res, next){ //req means requests, res means responds
+    res.json({'message': 'Hello!'})
+    // stop the serve and run it again so it updates the message
+})
+
+module.exports = router
