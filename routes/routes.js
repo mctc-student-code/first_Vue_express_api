4e1c3771b8c87f7381124cc0d0706acde028541b
RageKage <99547837+RageKage@users.noreply.github.com>
2022-11-04 14:38:21 -0500

    commit - Added Azure to the file

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', function(req, res, next){ //req means requests, res means responds
-    res.json({'message': 'Hello! Welcome to my Vue site'})
+    res.json({'message': 'Hello! This is the Azure version'})
     // stop the serve and run it again so it updates the message
 })
 
