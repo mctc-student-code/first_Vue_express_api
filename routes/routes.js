a57ea2622941a7bebb12656f47fa10dae74acbc1
RageKage <99547837+RageKage@users.noreply.github.com>
2022-11-08 16:42:19 -0600

    commit - checking to see if it works after updates

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', function(req, res, next){ //req means requests, res means responds
-    res.json({'message': 'Hello! This is the Azure version'})
+    res.json({'message': 'Hello! This is the Azure version, Checking to see if it works'})
     // stop the serve and run it again so it updates the message
 })
 
